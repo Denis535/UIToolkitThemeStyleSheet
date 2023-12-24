cdd966aecc7c9e69a150b15ac2d1e909160194c9
Denis <dddenisss35@gmail.com>
2023-12-24 20:20:34 +0200

    Example

---
+++ Assets/com.uitoolkit-theme-style-sheet/Samples/UIToolkit.ThemeStyleSheet/Example.cs
@@ -0,0 +1,24 @@
+namespace UIToolkit.ThemeStyleSheet {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UIElements;
+
+    public class Example : MonoBehaviour {
+
+        // Awake
+        public void Awake() {
+            var document = GetComponent<UIDocument>();
+        }
+        public void OnDestroy() {
+        }
+
+        // Start
+        public void Start() {
+        }
+        public void Update() {
+        }
+
+    }
+}
