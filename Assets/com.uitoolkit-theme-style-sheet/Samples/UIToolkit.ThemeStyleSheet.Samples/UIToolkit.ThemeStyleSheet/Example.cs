21d737363dcb00b8a68611b3c075e871d762ea24
Denis <dddenisss35@gmail.com>
2023-12-27 23:49:13 +0200

    Example

--- Assets/com.uitoolkit-theme-style-sheet/Samples/UIToolkit.ThemeStyleSheet.Samples/UIToolkit.ThemeStyleSheet/Example.cs
+++ Assets/com.uitoolkit-theme-style-sheet/Samples/UIToolkit.ThemeStyleSheet.Samples/UIToolkit.ThemeStyleSheet/Example.cs
@@ -8,6 +8,8 @@ namespace UIToolkit.ThemeStyleSheet {
 
     public class Example : MonoBehaviour {
 
+        [SerializeField] private Texture2D background;
+
         // Awake
         public void Awake() {
             var document = GetComponent<UIDocument>();
@@ -23,10 +25,22 @@ namespace UIToolkit.ThemeStyleSheet {
         }
 
         // Helpers
-        private static VisualElement CreateVisualElement() {
-            using (var root = VisualElementFactory.Widget().AsScope()) {
-                
-                
+        private VisualElement CreateVisualElement() {
+            using (var root = VisualElementFactory.Widget().Classes( "grow-1" ).Style( i => i.backgroundImage = new StyleBackground( background ) ).AsScope()) {
+                using (VisualElementFactory.LargeWidget().AsScope()) {
+                    using (VisualElementFactory.Card().AsScope()) {
+                        using (VisualElementFactory.Header().AsScope()) {
+                            VisualElementFactory.Label( "UIToolkit Theme Style Sheet" ).Classes( "medium" );
+                        }
+                        using (VisualElementFactory.Content().AsScope()) {
+
+                        }
+                        using (VisualElementFactory.Footer().AsScope()) {
+                            VisualElementFactory.Button( "Okey" );
+                            VisualElementFactory.Button( "Cancel" );
+                        }
+                    }
+                }
                 return root.VisualElement;
             }
         }
