8b01d11cb721eb484e778ee5bce3b5e0c7e241a0
Denis <dddenisss35@gmail.com>
2023-12-29 14:16:10 +0200

    Scroll-View

--- Assets/com.uitoolkit-theme-style-sheet/Samples/UIToolkit.ThemeStyleSheet.Samples/UIToolkit.ThemeStyleSheet/Example.cs
+++ Assets/com.uitoolkit-theme-style-sheet/Samples/UIToolkit.ThemeStyleSheet.Samples/UIToolkit.ThemeStyleSheet/Example.cs
@@ -26,21 +26,27 @@ namespace UIToolkit.ThemeStyleSheet {
 
         // Helpers
         private VisualElement CreateVisualElement() {
-            using (var root = VisualElementFactory.Widget().Classes( "grow-1" ).Style( i => i.backgroundImage = new StyleBackground( background ) ).AsScope()) {
+            using (var root = VisualElementFactory.Widget().Classes( "grow-1" ).Style( i => i.backgroundImage = background ).Style( i => i.unityBackgroundScaleMode = ScaleMode.StretchToFill ).AsScope()) {
                 using (VisualElementFactory.LargeWidget().AsScope()) {
                     using (VisualElementFactory.Card().AsScope()) {
                         using (VisualElementFactory.Header().AsScope()) {
                             VisualElementFactory.Label( "UIToolkit Theme Style Sheet" );
                         }
                         using (VisualElementFactory.Content().AsScope()) {
-                            using (VisualElementFactory.ScrollView().Classes( "dark", "grow-1" ).AsScope()) {
-                                for (var i = 0; i < 50; i++) {
-                                    var style = new[] { "light2", "light", "gray", "dark", "dark2" }[ i % 5 ];
-                                    using (VisualElementFactory.Box().Classes( style, "justify-content-center", "align-items-left", "width-200pc", "height-100px" ).AsScope()) {
-                                        VisualElementFactory.Label( style ).Classes( "medium" );
+                            // ScrollView
+                            using (VisualElementFactory.ScrollView().Classes( "dark", "medium", "grow-1" ).AsScope()) {
+                                var styles = new[] { "light2", "light", "gray", "dark", "dark2" };
+                                for (var y = 0; y < 30; y++) {
+                                    using (VisualElementFactory.RowScope().AsScope()) {
+                                        for (var x = 0; x < 30; x++) {
+                                            using (VisualElementFactory.Box().Classes( styles[ x % 5 ], "width-auto", "height-auto", "flex-0", "shrink-0", "justify-content-center", "align-items-center" ).AsScope()) {
+                                                VisualElementFactory.Label( styles[ x % 5 ] ).Classes( "medium" );
+                                            }
+                                        }
                                     }
                                 }
                             }
+
                         }
                         using (VisualElementFactory.Footer().AsScope()) {
                             VisualElementFactory.Button( "Submit" );
