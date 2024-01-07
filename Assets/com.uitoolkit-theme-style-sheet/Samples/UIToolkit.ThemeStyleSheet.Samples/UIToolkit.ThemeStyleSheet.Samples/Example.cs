b42a960e167930dcb400b4d8d2c8ee3fdd6b5c38
Denis <dddenisss35@gmail.com>
2024-01-07 19:23:16 +0200

    Style

--- Assets/com.uitoolkit-theme-style-sheet/Samples/UIToolkit.ThemeStyleSheet.Samples/UIToolkit.ThemeStyleSheet.Samples/Example.cs
+++ Assets/com.uitoolkit-theme-style-sheet/Samples/UIToolkit.ThemeStyleSheet.Samples/UIToolkit.ThemeStyleSheet.Samples/Example.cs
@@ -358,7 +358,7 @@ namespace UIToolkit.ThemeStyleSheet.Samples {
                                     VisualElementFactory.Button( "Button" ),
                                     VisualElementFactory.RepeatButton( "Repeat Button" ),
                                     VisualElementFactory.TextField( "Text Field", "Hello World !!!", 64, false ),
-                                    VisualElementFactory.ReadOnlyTextField( "Read Only Text Field", "Hello World !!!", 64, false ),
+                                    VisualElementFactory.ReadOnlyTextField( "Read Only Text Field", "Hello World !!!", 64, false ).Classes( "invalid" ),
                                     VisualElementFactory.PopupField( "Popup Field", "Item 1", new[] { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10" } ),
                                     VisualElementFactory.DropdownField( "Dropdown Field", "Item 1", new[] { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12", "Item 13", "Item 14", "Item 15", "Item 16", "Item 17", "Item 18", "Item 19", "Item 20" } ),
                                     VisualElementFactory.SliderField( "Slider Field", 1, 0, 2 ),
@@ -368,7 +368,7 @@ namespace UIToolkit.ThemeStyleSheet.Samples {
                             );
                         }
                         using (VisualElementFactory.Footer().AsScope()) {
-                            VisualElementScope.Add( VisualElementFactory.Button( "Submit" ), VisualElementFactory.Button( "Cancel" ) );
+                            VisualElementScope.Add( VisualElementFactory.Button( "Submit" ).Classes( "invalid" ), VisualElementFactory.Button( "Cancel" ) );
                         }
                     }
                     Document.rootVisualElement.Clear();
